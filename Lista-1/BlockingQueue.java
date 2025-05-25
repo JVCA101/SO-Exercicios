468a2ab341339c59b0f6c6a334b25915c3b58fcd
JVCA101 <joaovictor.correa@estudante.ufjf.com>
2025-05-25 08:59:24 -0300

    exercicio 4.1 terminado

--- Lista-1/BlockingQueue.java
+++ Lista-1/BlockingQueue.java
@@ -8,16 +8,15 @@ public class BlockingQueue<T> {
     private final int maxSize = 3;
 
     public synchronized void put(T item) throws InterruptedException {
-        while(queue.size() == maxSize)
+        if(queue.size() == maxSize) // bloqueia se cheio
             wait();
-        System.err.println("put");
         queue.add(item);
+        notifyAll();
     }
     
     public synchronized T take() throws InterruptedException {
-        while(queue.isEmpty())
+        if(queue.isEmpty()) // bloqueia se vazio
             wait();
-        System.err.println("take");
         T item = queue.poll();
         notifyAll();
         return item;
@@ -26,19 +25,39 @@ public class BlockingQueue<T> {
     public static void main(String[] args) {
         BlockingQueue<Integer> fila = new BlockingQueue<>();
 
-        System.err.println("Fila criada");
-
-        try{
+        Thread produtor = new Thread(() -> {
             for(int i = 0; i < 10; i++) {
-                fila.put(i);
-                Integer item = fila.take();
-                System.err.println("Consumido: " + item);
+                try {
+                    fila.put(i);
+                }
+                catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
+        });
+
+        Thread consumidor = new Thread(() -> {
+            for(int i = 0; i < 10; i++) {
+                try {
+                    int item = fila.take();
+                    System.err.println("Consumido: " + item);
+                }
+                catch(InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        produtor.start();
+        consumidor.start();
+
+        try {
+            produtor.join();
+            consumidor.join();
         }
         catch(InterruptedException e) {
-            System.err.println("Erro em inserir item");
+            e.printStackTrace();
         }
-        finally {}
 
     }
 }
