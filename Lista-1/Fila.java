1b4b4bf0ebe30399306a059f54e1e0c2f40e0b81
JVCA101 <joaovictor.correa@estudante.ufjf.com>
2025-05-25 15:24:47 -0300

    exercicio 5.4

---
+++ Lista-1/Fila.java
@@ -0,0 +1,20 @@
+// Exercicio 5.4
+
+// quando há múltiplos consumidores, alguns threads ficam em starvation porque quando adiciona um item à fila
+// apenas 1 thread é notificada(pela função notify()), a forma correta de se implementar é utilizando a função notifyAll()
+
+public class Fila {
+    private Queue<Integer> fila = new LinkedList<>();
+    
+    public synchronized void adicionar(int item) {
+        fila.add(item);
+        notifyAll(); // previamente notify()
+    }
+    
+    public synchronized int remover() throws InterruptedException {
+        while (fila.isEmpty()) {
+           wait();
+        }
+        return fila.poll();
+    }
+}
